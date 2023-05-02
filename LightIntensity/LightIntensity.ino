73cfaca1dae3f3f37c5323244dde544c6a598d8b
Abderahmane Mahdi Gharzouli <abderahmanemahdigharzouli@Abderahmanes-MacBook-Air.local>
2023-05-02 15:05:08 +0100

    first commit

---
+++ LightIntensity/LightIntensity.ino
@@ -0,0 +1,18 @@
+#include <Wire.h>
+#include <BH1750.h>
+
+BH1750 lightMeter;
+
+void setup() {
+  Serial.begin(115200);
+  Wire.begin();
+  lightMeter.begin();
+}
+
+void loop() {
+  float lux = lightMeter.readLightLevel();
+  Serial.print("Light intensity: ");
+  Serial.print(lux);
+  Serial.println(" lux");
+  delay(1000);
+}
